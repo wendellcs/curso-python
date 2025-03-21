4b394adde99cf39af8268f19e60db1eee82474dc
Wendell <wendellcs131@gmail.com>
2025-03-20 22:04:08 -0300

    Types of input and output

---
+++ gradio/app_types.py
@@ -0,0 +1,42 @@
+import gradio as gr
+def processar_dados(texto, numero, imagem, lista_texto, cor, opcoes):
+    texto_invertido = texto[::-1]
+    dobro_numero = numero * 2
+    mensagem_imagem = 'Imagem recebida!' if imagem else 'Nenhuma imagem recebida'
+    lista_processada = [[item] for item in lista_texto.splitlines()] if lista_texto else []
+
+    return (
+        texto_invertido,
+        dobro_numero,
+        mensagem_imagem,
+        lista_processada,
+        f'Cor selecionada: {cor}',
+        opcoes
+    )
+
+iface = gr.Interface(
+    fn=processar_dados,
+    inputs=[
+        gr.Textbox(label='Texto', placeholder='Digite um texto aqui'),
+        gr.Slider(minimum=0, maximum=100, label='Número', value=0),
+        gr.Image(type='pil', label='Imagem'),
+        gr.Textbox(label='Lista de itens', lines=4, placeholder='Item 1\nItem 2'),
+        gr.ColorPicker(label='Selecione uma cor'),
+        gr.CheckboxGroup(
+            choices=['Opção 1', 'Opção 2' , 'Opção 3'],
+            label='Escolha suas opções'
+        )
+    ],
+    outputs=[
+        gr.Textbox(label='Texto invertido'),
+        gr.Number(label='Dobro do número'),
+        gr.Textbox(label='Mensagem sobre a imagem'),
+        gr.DataFrame(label='Itens da lista', headers=['Itens']),
+        gr.Textbox(label='Cor selecionada'),
+        gr.Textbox(label='Opções selecionadas'),
+    ],
+    title='Verificador de tipos de entradas e saídas',
+    description='Insira um texto, número, imagem, lista de itens, cor e opções para ver como a entrada é processada.'
+)
+
+iface.launch()
